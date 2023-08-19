acb2e2e97c9dfa513b60c88c14fbdcb627117350
laxmikishore13 <laxmikishore1@gmail.com>
2023-08-19 21:34:48 +0530

    ui part mostly completed

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,22 +1,48 @@
-import { Box, Grid, TextField } from "@mui/material";
+/* eslint-disable react/prop-types */
+import { Delete, Done, Edit } from "@mui/icons-material";
+import { Box, Grid, InputAdornment, TextField } from "@mui/material";
+import { useState } from "react";
 
-function List() {
-  let items = [
-    { taks: "Complete todo app" },
-    { taks: "Complete todo app" },
-    { taks: "Complete todo app" },
-    { taks: "Complete todo app" },
-    { taks: "Complete todo app" },
-    { taks: "Complete todo app" },
-  ];
+function List(props) {
+  const [isEditable, setIsEditable] = useState(false);
   return (
     <>
       <Box sx={{ marginTop: 8 }}>
         <Grid container spacing={2}>
-          {items.map((item, index) => {
+          {props.tasks?.map((item, index) => {
             return (
               <Grid item xs={12} sm={10} key={index}>
-                <TextField fullWidth value={item.taks} key={index} />
+                <TextField
+                  fullWidth
+                  disabled={item.isDisabled}
+                  value={item.taks}
+                  key={index}
+                  InputProps={{
+                    endAdornment: (
+                      <>
+                        {!isEditable ? (
+                          <InputAdornment position="end">
+                            <Edit
+                              onClick={() => {
+                                props.handleTask(index);
+                                setIsEditable(true);
+                              }}
+                              sx={{ cursor: "pointer" }}
+                            />
+                          </InputAdornment>
+                        ) : null}
+                        <InputAdornment position="end">
+                          <Delete sx={{ cursor: "pointer" }} />
+                        </InputAdornment>
+                        {isEditable ? (
+                          <InputAdornment position="end">
+                            <Done sx={{ cursor: "pointer" }} />
+                          </InputAdornment>
+                        ) : null}
+                      </>
+                    ),
+                  }}
+                />
               </Grid>
             );
           })}
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,27 +1,56 @@
 import { Box, TextField, Container, Grid, Button } from "@mui/material";
 import List from "./List";
+import { useState } from "react";
 
 function Todo() {
+  const [text, setText] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleTask = (taskId) => {
+    setTasks((prevTask) =>
+      prevTask.map((task, index) => {
+        if (index === taskId) {
+          return {
+            ...task,
+            isDisabled: false,
+          };
+        }
+        return task;
+      })
+    );
+  };
   return (
     <Box sx={{ m: 20 }}>
       <Container component="main" maxWidth="md">
         <Grid container spacing={3} alignItems="center">
           <Grid item xs={12} sm={8}>
-            <TextField fullWidth label="What do you need to do?" />
+            <TextField
+              value={text}
+              fullWidth
+              label="What do you need to do?"
+              onChange={(e) => setText(e.target.value)}
+            />
           </Grid>
           <Grid item xs={12} sm={2}>
             <Button
               variant="contained"
               size="large"
               color="primary"
-              type="submit"
               sx={{ marginLeft: 4 }}
+              onClick={() => {
+                const obj = {
+                  taks: text,
+                  isDisabled: true,
+                };
+                setTasks((prev) => [...prev, obj]);
+                setText("");
+              }}
             >
               Add
             </Button>
           </Grid>
         </Grid>
-        {<List />}
+        {<List tasks={tasks} handleTask={(id) => handleTask(id)} />}
       </Container>
     </Box>
   );
